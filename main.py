42c362c194d9dc3166710cd7d7dc8572cecb5a77
Daniel Eduardo Cañón <daniel.canon@invytec.com>
2021-04-05 09:18:35 -0500

    clase: uso de metodo post en flask wtf

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import make_response
 from flask import redirect
 from flask import render_template
 from flask import session
+from flask import url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 # wtforms imports
@@ -48,14 +49,19 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form
-
+        'login_form': login_form,
+        'username': username
     } 
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        return redirect(url_for('index'))
     return render_template('hello.html', **context)
